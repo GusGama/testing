2249ffef51ec9a5f02ae916a96f6354690c5381c
Gustavo Gama <gustavogama@Gustavos-MacBook-Pro.local>
2018-10-28 17:22:30 -0600

    corre y salta

---
+++ Assets/scripts/movimiento_baldosa.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento_baldosa : gravity {
+    public float maxSpeed = 7;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 move = Vector2.zero;
+
+        //move.x = Input.GetAxis("Horizontal");
+        move.x = move.x - maxSpeed;
+    }
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        //move.x = Input.GetAxis("Horizontal");
+        move.x = move.x - maxSpeed;
+        //if (Input.GetButtonDown("Jump") && grounded)
+        
+
+        //bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        //if (flipSprite)
+        //{
+        //    spriteRenderer.flipX = !spriteRenderer.flipX;
+        //}
+
+        //animator.SetBool("grounded", grounded);
+        //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}
